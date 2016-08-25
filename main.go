eb7e36e6025fc05405b8f3addcbf38a0aec03f9e
Martin R <sizlaksystems@gmail.com>
2016-08-25 22:54:33 +0200

    Initial commit of demo-golang-drie-app

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type Message struct {
+	Message string `json:"message"`
+}
+
+func SayHelloDrieHandler(w http.ResponseWriter, r *http.Request) {
+
+	log.Println("GET /say-hello-drie")
+
+	message := Message{"Hello Golang Drie!"}
+
+	js, err := json.Marshal(message)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func main() {
+
+	http.HandleFunc("/say-hello-drie", SayHelloDrieHandler)
+	http.ListenAndServe(":8080", nil)
+}
